24b2a3b1b53c10249b93725c7ee7cb7f8003051a
Md Labib Sarkar <labibsarkar740268@gmail.com>
2026-08-13 01:48:08 +0600

    Initial commit

---
+++ 1.js
@@ -0,0 +1,15 @@
+console.log("hello javascript");
+
+
+//var,let,const
+//var diye re declare kora jay but 
+// let diye re declare kora jay na e
+// const diye variable er valu chgange hoy na
+var name1 ="Labib";
+var name1 ="Labib Sarkar";
+const income = 200.30667;
+console.log(name1);
+console.log(income.toFixed(3));cons
+//console.log(income); ja ase tai show korbe
+//console.log(income.toFixed(2)); .er por 2ta dekhabe
+//console.log(income.toPrecision(3)); suru theke tolal 3ta dekhabe
---
+++ scope3.js
@@ -0,0 +1,8 @@
+//Global scope
+let roll = "138";
+function showroll(){
+    console.log(roll)
+}
+showroll()
+let neme = "Labib";
+console.log( neme)
