142b9192c2cc20459b83789fef5b91f06854ae7a
Md Labib Sarkar <labibsarkar740268@gmail.com>
2026-09-08 02:23:38 +0600

    added practice_project file

---
+++ object_3/practice_project/details-obj.js
@@ -0,0 +1,5 @@
+//এই প্রজেক্টটিতে আমরা একটি Smart E-Commerce Product & Inventory Management System তৈরি করব।
+
+//এখানে Level 1 থেকে Level 5-এর সমস্ত প্রধান কনসেপ্ট (Nested Objects, Methods, this, Iteration,
+// Destructuring, Spread Operator, Shallow vs Deep Copy, Protection, Descriptors, Prototypes, এবং Map/Set)
+//  বাস্তবমুখী কোডে প্রয়োগ করা হয়েছে।
