6c6c359cfd040040e80ff48692cb87ad47d7d730
Md Labib Sarkar <labibsarkar740268@gmail.com>
2026-09-14 00:21:44 +0600

    updated Operators&Controlfow folder

--- ​Operators&Controlfow/1.js
+++ ​Operators&Controlfow/1.js
@@ -18,3 +18,55 @@ let ages = 20;
 let message = (age >= 18) ? "adult" : "not an adult";
 
 console.log(message); // Output: adult
+
+
+
+//Ternary Operator (if-else এর সংক্ষিপ্ত রূপ)
+//এক লাইনে ছোট শর্ত লেখার জন্য Ternary Operator (? :) খুব উপযোগী।
+
+let day = "Monday";
+
+switch (day) {
+  case "Sunday":
+    console.log("আজ ছুটির দিন!");
+    break;
+  case "Monday":
+    console.log("আজ সপ্তাহের প্রথম কর্মদিবস।");
+    break;
+  default:
+    console.log("সাধারণ একটি দিন।");
+}
+
+
+//২. Loops (একই কাজ বারবার করা)
+//for Loop (যখন লুপের সংখ্যা জানা থাকে)
+//আপনি কতবার লুপটি চালাতে চান তা জানা থাকলে for loop ব্যবহার করা হয়।
+
+
+
+// ১ থেকে ৫ পর্যন্ত প্রিন্ট করা
+for (let i = 1; i <= 5; i++) {
+  console.log("সংখ্যা: " + i);
+}
+
+
+//while Loop (যতক্ষণ শর্ত সত্য থাকবে)
+//নির্দিষ্ট একটি শর্ত যতক্ষণ সত্য (true) থাকবে, ততক্ষণ এই লুপ চলতে থাকবে।
+
+
+let count = 1;
+
+while (count <= 3) {
+  console.log("কাউন্ট: " + count);
+  count++; // শর্ত শেষ করার জন্য মান বাড়ানো হচ্ছে
+}
+
+//for...of Loop (Array বা লিস্টের উপাদানের জন্য)
+//কোনো Array (তালিকা) বা String-এর প্রতিটি উপাদানের ওপর দিয়ে একে একে লুপ চালাতে এটি খুব সহজ ও সুন্দর।
+
+
+let fruits = ["আম", "জাম", "কাঁঠাল"];
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
