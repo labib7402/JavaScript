5fd9c2c39c7b878042d193afa56c5c406167312a
labib7402 <labibsarkar740268@gmail.com>
2026-09-22 23:26:11 +0500

    added function folder

---
+++ functions/1.js
@@ -0,0 +1,21 @@
+// 1. ফাংশন তৈরির মূল গঠন (Syntax)
+//ফাংশন তৈরি করতে function কিওয়ার্ডটি ব্যবহার করা হয়:
+
+
+function functionName(parameter1, parameter2) {
+    // এখানে যে কাজ করানো হবে তার কোড থাকবে
+    return result; // (ঐচ্ছিক) কোনো মান ফেরত পাঠাতে চাইলে
+}
+
+
+
+//2. সাধারণ একটি ফাংশন (Basic Function)
+//নিচে একটি সাধারণ ফাংশনের উদাহরণ দেওয়া হলো যা কেবল একটি বার্তা প্রিন্ট করে:
+
+// ১. ফাংশন ঘোষণা (Function Declaration)
+function sayHello() {
+    console.log("হ্যালো! JavaScript-এ স্বাগতম।");
+}
+
+// ২. ফাংশন কল (Function Call / Invocation)
+sayHello(); // আউটপুট: হ্যালো! JavaScript-এ স্বাগতম।
