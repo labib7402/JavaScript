47fbcfaee94e62c301fa8e07e54900f29b750128
labib7402 <labibsarkar740268@gmail.com>
2026-09-23 00:41:06 +0600

    updated functions folder

--- functions/1.js
+++ functions/1.js
@@ -12,10 +12,24 @@ function functionName(parameter1, parameter2) {
 //2. সাধারণ একটি ফাংশন (Basic Function)
 //নিচে একটি সাধারণ ফাংশনের উদাহরণ দেওয়া হলো যা কেবল একটি বার্তা প্রিন্ট করে:
 
-// ১. ফাংশন ঘোষণা (Function Declaration)
+// a. ফাংশন ঘোষণা (Function Declaration)
 function sayHello() {
     console.log("হ্যালো! JavaScript-এ স্বাগতম।");
 }
 
-// ২. ফাংশন কল (Function Call / Invocation)
+// b. ফাংশন কল (Function Call / Invocation)
 sayHello(); // আউটপুট: হ্যালো! JavaScript-এ স্বাগতম।
+
+
+
+//3. প্যারামিটার ও আর্গুমেন্ট (Parameters and Arguments)
+//ফাংশনে বাহির থেকে ডাটা বা মান পাঠাতে Parameters এবং Arguments ব্যবহার করা হয়।
+
+// name হলো Parameter
+function greetUser(name) {
+    console.log("স্বাগতম, " + name + "!");
+}
+
+// "রাহাত" এবং "নাসরিন" হলো Arguments
+greetUser("রাহাত");  // আউটপুট: স্বাগতম, রাহাত!
+greetUser("নাসরিন"); // আউটপুট: স্বাগতম, নাসরিন!
